08acd7c98ce3be8b07233ac66985494f7ab1e89b
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-02-02 02:01:18 +0400

    Header scroll sticky funcison add

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,8 +7,29 @@ import "./style.scss";
 
 
 const Header = () => {
+
+  //! header scroll ederken fonksiyonluq
+  const [lastScrollTop, setLastScrollTop] = React.useState(0);
+  const [visible, setVisible] =React. useState(true);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      let currentScrollPos = window.pageYOffset
+      if(currentScrollPos > lastScrollTop || (currentScrollPos == 0)) {
+        setVisible(false)
+      } else {
+        setVisible(true)
+      }
+      setLastScrollTop(currentScrollPos < 0 ? 0 : currentScrollPos);
+    }
+    
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollTop])
+
+
   return (
-    <header className="header fluid">
+    <header className={`header fluid ${visible ? "visible" : ""}`}>
       <div className="left_header">
         <Image src="/assets/icon/Logo.svg" width={10} height={50} alt="Logo" />
       </div>
