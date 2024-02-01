590555e02663f7bbaede5648b30a101bd112eb1d
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-02-02 01:52:27 +0400

    Update bug fixed

---
+++ src/app/admin/products/(loading)/loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FadeLoader from 'react-spinners/FadeLoader'
+import '../../../(client)/[[...categoriesId]]/loading.scss'
+
+const Loading = () => {
+
+  return (
+    <div className="loading-container">
+      <div className="loading">
+        <FadeLoader color="#36d7b7" height={18} margin={2} radius={14} width={4} />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
