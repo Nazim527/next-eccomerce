65248539936b8c2cb68bf3878a7b22f73f8d583c
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-02-02 14:06:33 +0400

    Toastify add

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "@/helpers/reset.scss";
 import { Providers } from "@/lib/StoreProvider";
 import '../../helpers/reset.scss'
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children } ) {
       <body className={`${inter.className} container`} style={{height:"100vh", overflow: "hidden"}}>
         <Providers>
           {children}
+          <ToastContainer/>
         </Providers>
       </body>
     </html>
