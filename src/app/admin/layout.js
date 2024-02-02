cfb18b6dab475e40157dbe2f52b89b7d1c05a98d
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-02-02 20:08:18 +0400

    Head add

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "@/helpers/reset.scss";
 import { Providers } from "@/lib/StoreProvider";
 import '../../helpers/reset.scss'
+import Head from "../Head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children } ) {
   return (
     <html lang="en">
+      <Head/>
       <body className={`${inter.className} container`}>
         <Providers>
           {children}
